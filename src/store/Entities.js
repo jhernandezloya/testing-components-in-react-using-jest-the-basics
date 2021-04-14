bdcceb9f6af2ff63546404371f4edf341c68a4b1
Julio Daniel Hernandez Loya <julio.hernandezl@coopeuch.cl>
2021-04-14 00:33:14 -0400

    enviando cambios para los archivos con cambios

--- src/store/Entities.js
+++ src/store/Entities.js
@@ -1,36 +1,5 @@
+import GenericClass from './GenericClass';
 
-class GenericClass {
-  replaceAttribute(item)  {
-    
-    const mapToObj = m => {
-      return Array.from(m).reduce((obj, [key, value]) => {
-        const elemet = obj;
-        elemet[key] = value;
-        return elemet;
-      }, {});
-    };
-
-    const newElementJSON = JSON.stringify(mapToObj(Object.entries(item)));
-    const itemElemento = JSON.parse(newElementJSON);
-
-  
-    const newElement1 = new Map();
-    Object.entries(this).forEach(([key, value]) =>
-      {
-        if(typeof value!=='undefined'){
-          newElement1.set(key, value)
-        }
-      }
-    );
-
-    
-    const newElementJSON1 = JSON.stringify(mapToObj(newElement1));
-    const itemElemento2 = JSON.parse(newElementJSON1); 
-
-    const salida = Object.assign(itemElemento,itemElemento2);
-    return Object.assign(this,salida);
-  }
-}
 export class Oferta extends GenericClass {
 
   constructor(rut,monto,tasa,montoLiquido,cae)  {
